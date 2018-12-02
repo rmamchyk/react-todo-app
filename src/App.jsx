16f627ed7da142cd7843df928e1810209f5e26f0
rmamchyk <roman.mamchyk@gmail.com>
2018-12-03 00:15:01 +0200

    Added validation for App props by using 'prop-types' package

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+function App(props) {
     return (
         <main>
             <header>
-                <h1>React Todo</h1>
+                <h1>{props.title}</h1>
             </header>
+
             <section className="todo-list">
+                <div className="todo completed">
+                    <button className="checkbox icon">
+                        <i className="material-icons">check_box</i>
+                    </button>
+                    <span className="todo-title">Learn JavaScript</span>
+                    <button className="delete icon">
+                        <i className="material-icons">delete</i>
+                    </button>
+                </div>
                 <div className="todo">
                     <button className="checkbox icon">
                         <i className="material-icons">check_box_outline_blank</i>
@@ -16,18 +27,17 @@ function App() {
                         <i className="material-icons">delete</i>
                     </button>
                 </div>
-                <div className="todo">
-                    <button className="checkbox icon">
-                        <i className="material-icons">check_box_outline_blank</i>
-                    </button>
-                    <span className="todo-title">Learn Redux</span>
-                    <button className="delete icon">
-                        <i className="material-icons">delete</i>
-                    </button>
-                </div>
             </section>
         </main>
     );
-}
+};
+
+App.propTypes = {
+    title: PropTypes.string
+};
+
+App.defaultProps = {
+    title: 'React Todo'
+};
 
 export default App;
